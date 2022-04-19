309d8948211791c062cf40fefa1eb3b24166b7ff
TheGoodlike13 <henrisssss@hotmail.com>
2022-04-19 17:25:36 +0300

    Documentation

--- src/eu/goodlike/oblivion/core/Effect.java
+++ src/eu/goodlike/oblivion/core/Effect.java
@@ -1,16 +1,20 @@
 package eu.goodlike.oblivion.core;
 
+import eu.goodlike.oblivion.global.Settings;
+
 /**
- * Active effect that influences or damages the target. The magnitude of this effect is snapshot upon activation and
- * can no longer be influenced until the effect is removed. The duration of this effect expires via ticking.
+ * Active effect that influences the target.
+ * The exact effect depends on {@link Type}.
+ * The magnitude of this effect is snapshot upon activation and can no longer be changed until the effect is removed.
+ * The duration of this effect expires via ticking.
  * <p/>
  * Not to be confused with {@link EffectText}, which describes base values before any multipliers come into play.
  */
 public interface Effect {
 
   /**
-   * Marker interface to uniquely identify the active effect. Ensures effects from the same source (e.g. spell) do not
-   * stack.
+   * Marker interface to uniquely identify the active effect.
+   * Ensures effects from the same source (e.g. spell) do not stack.
    */
   interface Id {
   }
@@ -36,12 +40,43 @@ public interface Effect {
     }
   }
 
+  /**
+   * If this effect is persistent (e.g. weakness to magic), adds the influence to the given target.
+   * For specifics, refer to the {@link Type} of the effect.
+   * <p/>
+   * Should be called when the active effect is applied to the target.
+   * <p/>
+   * Undefined if target is null.
+   */
   void onApply(Target target);
 
+  /**
+   * If this effect is persistent (e.g. weakness to magic), removes the influence from the given target.
+   * For specifics, refer to the {@link Type} of the effect.
+   * <p/>
+   * Should be called when the active effect is removed from the target.
+   * This method will NOT be called automatically upon expiration.
+   * <p/>
+   * Undefined if target is null.
+   */
   void onRemove(Target target);
 
+  /**
+   * Reduces the duration of this active effect by a single {@link Settings#TICK}.
+   * If this effect is continuous (e.g. damage over time), influences the target in proportion to the tick.
+   * If the remaining duration is less than a tick, the influence is proportional to the remaining duration instead.
+   * For specifics, refer to the {@link Type} of the effect.
+   * <p/>
+   * Should be called for every tick while the effect is active.
+   * Has no effect if the effect is expired.
+   * <p/>
+   * Undefined if target is null.
+   */
   void onTick(Target target);
 
+  /**
+   * @return false if this effect still has duration remaining, true otherwise
+   */
   boolean hasExpired();
 
 }
