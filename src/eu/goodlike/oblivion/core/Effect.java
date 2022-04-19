a7971735645f74327ee77b948af30e4cce055698
TheGoodlike13 <henrisssss@hotmail.com>
2022-04-19 16:59:01 +0300

    Documentation

--- src/eu/goodlike/oblivion/core/Effect.java
+++ src/eu/goodlike/oblivion/core/Effect.java
@@ -1,12 +1,36 @@
 package eu.goodlike.oblivion.core;
 
+/**
+ * Active effect that influences or damages the target. The magnitude of this effect is snapshot upon activation and
+ * can no longer be influenced until the effect is removed. The duration of this effect expires via ticking.
+ * <p/>
+ * Not to be confused with {@link EffectText}, which describes base values before any multipliers come into play.
+ */
 public interface Effect {
 
+  /**
+   * Marker interface to uniquely identify the active effect. Ensures effects from the same source (e.g. spell) do not
+   * stack.
+   */
   interface Id {
   }
 
+  /**
+   * Categorizes the effects and the way they affect the target.
+   * <p/>
+   * Can be considered a factory for active effects.
+   */
   interface Type {
+    /**
+     * @param magnitude effective magnitude of the new effect, after all multipliers are taken into account
+     * @param duration full duration of the new effect
+     * @return an active effect of this type with full duration
+     */
     Effect activate(double magnitude, double duration);
+
+    /**
+     * @return true if this type of effect influences or damages hp, false otherwise
+     */
     default boolean affectsHp() {
       return true;
     }
