7366eeb20710e9c3b08d8494d9145ff573be6cc0
TheGoodlike13 <henrisssss@hotmail.com>
2022-04-24 14:06:07 +0300

    Baseline in/out reader

--- src/eu/goodlike/oblivion/OblivionSpellStackingCalculator.java
+++ src/eu/goodlike/oblivion/OblivionSpellStackingCalculator.java
@@ -1,9 +1,44 @@
 package eu.goodlike.oblivion;
 
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public final class OblivionSpellStackingCalculator {
 
   public static void main(String... args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      OblivionSpellStackingCalculator c = new OblivionSpellStackingCalculator(scanner::nextLine, System.out::print);
+      c.intro();
+      c.run();
+    }
+  }
 
+  public void run() {
+    write("You entered: " + read());
+  }
+
+  public void intro() {
+    write("Welcome!");
+    write("Please select a target, cast some spells or perform attacks and GO!");
+    write("You can quit any time ;)");
+  }
+
+  public OblivionSpellStackingCalculator(Supplier<String> input, Consumer<String> output) {
+    this.input = input;
+    this.output = output;
+  }
+
+  private final Supplier<String> input;
+  private final Consumer<String> output;
+
+  private String read() {
+    output.accept(">> ");
+    return input.get().toLowerCase();
+  }
+
+  private void write(String line) {
+    output.accept(line + System.lineSeparator());
   }
 
 }
---
+++ test/eu/goodlike/oblivion/OblivionSpellStackingCalculatorTest.java
@@ -0,0 +1,57 @@
+package eu.goodlike.oblivion;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OblivionSpellStackingCalculatorTest implements Supplier<String>, Consumer<String> {
+
+  private final OblivionSpellStackingCalculator calc = new OblivionSpellStackingCalculator(this, this);
+
+  private Iterator<String> input;
+  private List<String> output;
+
+  @Override
+  public String get() {
+    return input.next();
+  }
+
+  @Override
+  public void accept(String s) {
+    if (!">> ".equals(s)) {
+      output.add(s.trim());
+    }
+  }
+
+  @BeforeEach
+  void setup() {
+    input = null;
+    output = new ArrayList<>();
+  }
+
+  @Test
+  void test() {
+    mockInput("anything");
+
+    calc.run();
+
+    assertOutput("You entered: anything");
+  }
+
+  private void mockInput(String... lines) {
+    input = Stream.of(lines).iterator();
+  }
+
+  private void assertOutput(String... lines) {
+    assertThat(output).containsExactly(lines);
+  }
+
+}
