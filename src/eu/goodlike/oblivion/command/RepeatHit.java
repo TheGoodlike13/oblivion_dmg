8d630c9cb2a7b0d4ab8ffa7d0e42f7b38cf22a10
TheGoodlike13 <henrisssss@hotmail.com>
2022-05-02 22:29:16 +0300

    Hit command usage simplified

--- src/eu/goodlike/oblivion/command/RepeatHit.java
+++ src/eu/goodlike/oblivion/command/RepeatHit.java
@@ -9,7 +9,7 @@ import static eu.goodlike.oblivion.Global.THE_ARENA;
 
 /**
  * Convenience command which allows repeating previous hits, especially multiple times.
- * Any input with '#' prefix will be treated as reference to hit to add next.
+ * Any input with or without '#' prefix will be treated as reference to hit to add next.
  * Any input with 'x' prefix will be treated as a multiplier for previous hit in this command.
  * 'x1' does nothing, as the command will have already added the hit.
  * 'x0' will NOT undo the hit.
@@ -30,12 +30,9 @@ public final class RepeatHit extends BaseCommand {
       int times = StructureException.intOrThrow(ref.substring(1), "repeat count");
       repeatLastHit(times);
     }
-    else if (ref.startsWith("#")) {
-      lastRef = ref.substring(1);
-      addHit();
-    }
     else {
-      throw new StructureException("Unexpected param", ref);
+      lastRef = ref.startsWith("#") ? ref.substring(1) : ref;
+      addHit();
     }
   }
 
